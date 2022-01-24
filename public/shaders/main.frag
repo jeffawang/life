596c9c352055669a20d66fc212fad2ab15a3ae0d
Jeff Wang <jeffawang@gmail.com>
2022-01-24 09:44:51 -0800

    yay, 2 shaders connected

--- public/shaders/main.frag
+++ public/shaders/main.frag
@@ -7,20 +7,18 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 uniform float u_time;
-
-float random (vec2 st) {
-    return fract(sin(dot(st.xy,
-                         vec2(12.9898,78.233)))*
-        43758.5453123);
-}
+uniform sampler2D u_texture;
 
 void main() {
+
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
     st.x *= u_resolution.x/u_resolution.y;
 
     vec3 color = vec3(0.);
     color = vec3(st.x,st.y,abs(sin(u_time)));
-    color = vec3(random(st));
 
-    gl_FragColor = vec4(color,1.0);
+    vec4 texSample = texture2D(u_texture, st);
+    texSample.r = st.x;
+
+    gl_FragColor = texSample;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import * as THREE from 'three';
 import "./index.css";
 
-let camera, scene, renderer;
+let camera, scene, renderer, lifeRenderer;
+let lifeScene, lifeRenderTarget;
+
+const mainUniforms = {
+  u_time: { value: 0 },
+  u_resolution:  { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+  u_frame: {value: 0 },
+  u_texture: {value: null}
+};
 
 const uniforms = {
   u_time: { value: 0 },
@@ -9,24 +17,38 @@ const uniforms = {
   u_frame: {value: 0 },
 };
 
-let fragmentShader, vertexShader;
-
-fetch("/shaders/life.frag")
+let lifeShader, mainShader;
+fetch("/shaders/main.frag")
   .then((r) => r.text())
-  .then((t) => fragmentShader = t)
-  .then(init)
-  .then(img)
+  .then((t) => mainShader = t)
+  .then(() => {
+    fetch("/shaders/life.frag")
+      .then((r) => r.text())
+      .then((t) => lifeShader = t)
+      .then(init)
+      .then(img)
+      .then(life)
+  });
+
+let texture;
 
 function init() {
   // Boilerplate: camera, scene, renderer
   camera = new THREE.OrthographicCamera(-1,1,1,-1,-1,1);
   scene = new THREE.Scene();
 
+
+  texture = new THREE.TextureLoader().load('noise.jpg');
+
   renderer = new THREE.WebGLRenderer({antialias: true});
+  lifeRenderTarget = new THREE.WebGLRenderTarget(document.body.clientWidth, document.body.clientHeight);
+  lifeScene = new THREE.Scene();
   document.body.appendChild( renderer.domElement );
   function resize() {
     renderer.setSize( document.body.clientWidth, document.body.clientHeight );
+    lifeRenderTarget.setSize( document.body.clientWidth, document.body.clientHeight );
     uniforms.u_resolution.value.set(document.body.clientWidth, document.body.clientHeight);
+    mainUniforms.u_resolution.value.set(document.body.clientWidth, document.body.clientHeight);
   }
   resize()
   window.addEventListener('resize', resize, false);
@@ -39,7 +61,29 @@ function init() {
 function main( time, frame ) {
   uniforms.u_time.value = time * 0.001;
   uniforms.u_frame.value = frame;
+
+  mainUniforms.u_time.value = time * 0.001;
+  mainUniforms.u_frame.value = frame;
+
+  // populate buffer
+  renderer.setRenderTarget(lifeRenderTarget);
+  renderer.render( lifeScene, camera );
+
+  // render to canvas
+  renderer.setRenderTarget(null);
+  mainUniforms.u_texture.value = lifeRenderTarget.texture;
   renderer.render( scene, camera );
+}
+
+function life() {
+  // PlaneBufferGeometry is deprecated, use PlaneGeometry in the future?
+  const plane = new THREE.PlaneBufferGeometry(2,2);
+
+  const material = new THREE.ShaderMaterial({
+    fragmentShader: lifeShader,
+    uniforms,
+  });
+  lifeScene.add(new THREE.Mesh(plane, material));
 }
 
 // img defines what finally gets rendered to the scene.
@@ -47,8 +91,8 @@ function img() {
   const plane = new THREE.PlaneBufferGeometry(2,2);
 
   const material = new THREE.ShaderMaterial({
-    fragmentShader,
-    uniforms,
+    fragmentShader: mainShader,
+    uniforms: mainUniforms,
   });
 
   scene.add(new THREE.Mesh(plane, material));
